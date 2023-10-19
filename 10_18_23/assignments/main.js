2a70e972c28ec1906d9113c3652defcbae555f24
Tom Spitz <tjspitz1986@gmail.com>
2023-10-19 09:28:50 -0500

    Experimenting with options and conditions

--- 10_18_23/assignments/main.js
+++ 10_18_23/assignments/main.js
@@ -1,11 +1,92 @@
+const btnYes = '#37bc37';
+const btnNo = '#efefef';
+let countSpaces = false;
+let countNums = false;
+let countSymbols = false;
+let countCases = false;
+
+const setSpaces = () => {
+  let thisOption = document.querySelector('#count-spaces');
+  countSpaces = !countSpaces;
+
+  if (countSpaces) {
+    thisOption.style = `background-color: ${btnYes};`;
+  } else {
+    thisOption.style = `background-color: ${btnNo};`;
+  }
+};
+
+const setNums = () => {
+  let thisOption = document.querySelector('#count-nums');
+  countNums = !countNums;
+
+  if (countNums) {
+    thisOption.style = `background-color: ${btnYes};`;
+  } else {
+    thisOption.style = `background-color: ${btnNo};`;
+  }
+};
+
+const setSymbols = () => {
+  let thisOption = document.querySelector('#count-symbols');
+  countSymbols = !countSymbols;
+
+  if (countSymbols) {
+    thisOption.style = `background-color: ${btnYes};`;
+  } else {
+    thisOption.style = `background-color: ${btnNo};`;
+  }
+};
+
+const setCases = () => {
+  let thisOption = document.querySelector('#count-cases');
+  countCases = !countCases;
+
+  if (countCases) {
+    thisOption.style = `background-color: ${btnYes};`;
+  } else {
+    thisOption.style = `background-color: ${btnNo};`;
+  }
+};
+
+const resetOptions = () => {
+  countSpaces = false;
+  countNums = false;
+  counSymbols = false;
+  countCases = false;
+};
+
 const measureMessage = (message) => {
   const charCount = {};
 
+  if (countCases) {
+    message = message.toLowerCase();
+  }
+
   for (let i = 0; i < message.length; i ++) {
     let currentChar = message[i];
+    let curCharCode = currentChar.charCodeAt(0);
 
-    if (currentChar === ' ') {
+    if (!countSpaces && curCharCode === 32) {
       continue;
+    } else {
+      currentChar = 'space_chars';
+    }
+
+    if (!countNums)  {
+      if (curCharCode > 47 && curCharCode < 58) {
+        continue;
+      }
+    }
+
+    if (!countSymbols) {
+      if (
+        curCharCode < 65 ||
+        (curCharCode >  90 && curCharCode < 97) ||
+        curCharCode > 122
+        ) {
+          continue;
+        }
     }
 
     if (charCount[currentChar]) {
@@ -15,6 +96,7 @@ const measureMessage = (message) => {
     }
   }
 
+  console.log(charCount);
   return charCount;
 };
 
@@ -32,6 +114,7 @@ const renderMessage = () => {
   charCountHTML += "</ul>"
   ans.innerHTML = "Your message contains the following characters:"
   ans.innerHTML += charCountHTML;
+  resetOptions();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
