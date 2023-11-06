160c7170901fd19e667c39774b9f34e8b9485d4e
Tom Spitz <tjspitz1986@gmail.com>
2023-11-06 11:31:46 -0600

    prettify it all

--- reactSprint/11_6_23/assignments/src/App.jsx
+++ reactSprint/11_6_23/assignments/src/App.jsx
@@ -1,14 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Unless of course you know how to edit your scripts...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
