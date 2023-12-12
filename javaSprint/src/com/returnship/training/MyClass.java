5614650cb6256b73d115ba0a5e76274b0038d7a8
Tom Spitz <tjspitz1986@gmail.com>
2023-12-12 13:49:55 -0600

    Implement the 'lucky number' assignment in Java

--- javaSprint/src/com/returnship/training/MyClass.java
+++ javaSprint/src/com/returnship/training/MyClass.java
@@ -2,14 +2,11 @@ package com.returnship.training;
 
 public class MyClass {
 	public static void main(String args[]) {
-		Examples_12_12_23.examples();
-		
+//		Examples_12_12_23.examples();
+		Counter.playGame();
 	}
 	
 	public static String greeting() {
 		return "Hello from Java";
 	}
-	
 }
-
-// assignment: make a lucky number guesser (using Scanner inputs)
