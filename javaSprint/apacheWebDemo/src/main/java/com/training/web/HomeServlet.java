d1796f04c4ac2fc40be8639792ae68fdc9da91eb
Tom Spitz <tjspitz1986@gmail.com>
2024-01-08 13:41:40 -0600

    today's noodles, setting up Apache server w/ servlets

---
+++ javaSprint/apacheWebDemo/src/main/java/com/training/web/HomeServlet.java
@@ -0,0 +1,52 @@
+package com.training.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<html><body><h1>Hello from Servlet...</h1></body></html>");
+        
+//        response
+//            .getWriter()
+//            .append("Served at: ")
+//            .append(request.getContextPath());
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
