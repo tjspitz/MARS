25cc73b56c142b457ea0ed2f1a1847403952ebd4
Tom Spitz <tjspitz1986@gmail.com>
2024-01-23 14:48:42 -0600

    very WET approach; refactoring User and its controller

--- elp/src/main/java/com/mars/elp/springboot/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mars.elp.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mars.elp.springboot.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
-    public Admin findByLastName(String name);
-}
