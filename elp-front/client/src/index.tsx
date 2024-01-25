bdd1c88ec31f4a5234fcb86aab1238f116fb1c56
Tom Spitz <tjspitz1986@gmail.com>
2024-01-25 13:45:11 -0600

    remove tester

--- elp-front/client/src/index.tsx
+++ elp-front/client/src/index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App';
+const mountNode = document.getElementById('app') as Element;
+const root = createRoot(mountNode);
 
-var mountNode = document.getElementById('app');
-ReactDOM.render(<App name="Banapants Steve" />, mountNode);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
