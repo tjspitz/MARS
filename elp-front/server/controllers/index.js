28e6e838050344c2055630303104bea805a5769d
Tom Spitz <tjspitz1986@gmail.com>
2024-01-26 12:43:31 -0600

    Peel off unnecesary backend routes & controllers We can hit the backend from the client

--- elp-front/server/controllers/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  users: require('./users'),
-  courses: require('./courses'),
-  feedback: require('./feedback')
-};
--- elp-front/server/index.js
+++ elp-front/server/index.js
@@ -1,16 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const router = require('./routes');
 const { PORT } = process.env;
 
-app.use(morgan('dev'));
-app.use(express.json());
-app.use('/api', router);
 app.use(express.static(path.join(__dirname, '../client/dist')));
-
 app.listen(PORT, () => {
   console.log(`elp-front server listening at http://localhost:${PORT}`);
 });
